e207d65bd0fb29a1efecdd3475acc5ffb8246065
Ginura Ransika De Seram <ginuraransika@gmail.com>
2023-02-20 16:28:08 +0530

    Added the OTP Verification Part

--- lib/src/features/authentication/controllers/on_boarding_controller.dart
+++ lib/src/features/authentication/controllers/on_boarding_controller.dart
@@ -1,7 +1,4 @@
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:kcroz/src/features/authentication/screens/welcome/welcome_screen.dart';
 import 'package:liquid_swipe/PageHelpers/LiquidController.dart';
 import '../../../constants/colors.dart';
@@ -47,13 +44,13 @@ class OnBoardingController extends GetxController{
 
 
 
-  skip() => Get.to(WelcomeScreen());
+  skip() => Get.to(const WelcomeScreen());
   animateToNextSlide() {
     int nextPage = controller.currentPage + 1;
     if(controller.currentPage != 2) {
       controller.animateToPage(page: nextPage);
     } else {
-      Get.to(WelcomeScreen());
+      Get.to(const WelcomeScreen());
     }
   }
   onPageChangedCallback(int activePageIndex) {
