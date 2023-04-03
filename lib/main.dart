8a911102282abd0620728c0575af6f8a4d6820d0
Abdullah Marikkar <abdullahmarikkar2@gmail.com>
2023-04-03 21:11:37 +0530

    additional_step changes
    Made additional_step to check location details and update it to the firestore every 1 minute after a user signs up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/transitions_type.dart';
@@ -38,7 +39,7 @@ void main() async{
         options: DefaultFirebaseOptions.currentPlatform
     );
   }
-
+  await Geolocator.requestPermission();
   runApp(const MyApp());
 }
 
