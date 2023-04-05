e71afe73b075f5d025ad12ddb5cda3803d492f43
Abdullah Marikkar <abdullahmarikkar2@gmail.com>
2023-04-05 08:47:47 +0530

    Added update location functions to home page
    Added functions to update current location of the user every 1 minute when he directed towards home page of map

--- lib/src/features/core/screens/home_screen_items.dart
+++ lib/src/features/core/screens/home_screen_items.dart
@@ -4,7 +4,7 @@ import 'package:kcroz/src/features/core/screens/map/home_page.dart';
 import 'package:kcroz/src/features/core/screens/profile/profile_screen.dart';
 
 List<Widget> homeScreenItems = [
-  home_page(),
+  Home_page(),
   Center(child: Text("Game")),
   Center(child: Text("Message")),
   ProfileScreen(),
